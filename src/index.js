3848fb838a140a829d14bd374af97ceccf81fd04
Konstantin <shuvalov.2021@list.ru>
2021-02-17 09:02:22 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let reverseN = n.toString().split('')
+  if(reverseN[0] === '-') {
+      reverseN.shift();
+  }
+  console.log(reverseN);
+return Number(reverseN.reverse().join(''));
 }
